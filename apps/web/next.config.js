9181ce50aa5b632df9cbe6b7ca11d6e969f6f4a8
Sajal Agrawal <59311163+sajal101agrawal@users.noreply.github.com>
2026-09-09 14:21:23 +0530

    security: remove malicious script appended by compromised token

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,8 @@ import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
